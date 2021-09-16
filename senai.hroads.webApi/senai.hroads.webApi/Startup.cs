53b38fe4652c07f062ceb42ba819cfee3e004350
Gustavo Lima <glima.0913@gmail.com>
2021-09-16 15:31:45 -0300

    Estrutura da API criada

---
+++ senai.hroads.webApi/senai.hroads.webApi/Startup.cs
@@ -0,0 +1,110 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace senai.hroads.webApi
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Define o uso de Controllers
+            services
+                .AddControllers()
+
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                });
+
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "senai.hroads.webApi", Version = "v1" });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            services
+                // Define a forma de autenticação
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = "JwtBearer";
+                    options.DefaultChallengeScheme = "JwtBearer";
+                })
+
+                // Define os parâmetros de validação do token
+                .AddJwtBearer("JwtBearer", options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        // Valida quem está solicitando
+                        ValidateIssuer = true,
+
+                        // Valida quem está recebendo
+                        ValidateAudience = true,
+
+                        // Define se o tempo de expiração será validado
+                        ValidateLifetime = true,
+
+                        // Forma de criptografia e ainda valida a chave de autenticação
+                        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("aindaadecidir")),
+
+                        // Valida o tempo de expiração do token
+                        ClockSkew = TimeSpan.FromMinutes(30),
+
+                        // Nome do issuer, de onde está vindo
+                        ValidIssuer = "senai.hroads.webApi",
+
+                        // Nome do audience, para onde está indo
+                        ValidAudience = "senai.hroads.webApi"
+                    };
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "senai.hroads.webApi");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseRouting();
+
+            // Habilita a autenticação
+            app.UseAuthentication();
+
+            // Habilita a autorização
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                // Define o mapeamento dos Controllers
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
