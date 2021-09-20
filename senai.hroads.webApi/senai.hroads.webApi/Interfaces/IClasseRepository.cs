5beca18adc67ecdb8f68a67b66a5d171e1c1dbb9
Zeck <61870100+z-eck@users.noreply.github.com>
2021-09-20 13:15:48 -0300

    Add files via upload

---
+++ senai.hroads.webApi/senai.hroads.webApi/Interfaces/IClasseRepository.cs
@@ -0,0 +1,55 @@
+﻿using senai.hroads.webApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.hroads.webApi.Interfaces
+{
+    interface IClasseRepository
+    {
+        /// <summary>
+        /// Lista todas as classes
+        /// </summary>
+        /// <returns>A lista com todas as classes</returns>
+        List<Classe> ListarTodos();
+
+        /// <summary>
+        /// Busca pelo ID uma classe em específica
+        /// </summary>
+        /// <param name="idClasse">ID da classe que será utilizada para a busca</param>
+        /// <returns>A classe buscada pela ID</returns>
+        Classe BuscarPorID(int idClasse);
+
+        /// <summary>
+        /// Registra uma nova classe
+        /// </summary>
+        /// <param name="novaClasse">Objeto utilizado para nomear a nova classe</param>
+        void Cadastrar(Classe novaClasse);
+
+        /// <summary>
+        /// Atualizará uma informação, sendo buscada pela URL
+        /// </summary>
+        /// <param name="idClasse">ID da classe que será utilizada para a identificação de qual ID que será atualizada</param>
+        /// <param name="classeAtualizada">Objeto que estará no corpo da requisição que servirá para ser atualizado</param>
+        void AtualizarURL(int idClasse, Classe classeAtualizada);
+
+        /// <summary>
+        /// Deletará uma classe
+        /// </summary>
+        /// <param name="idClasse">ID da classe que será utilizada para a remoção de uma informação</param>
+        void Deletar(int idClasse);
+
+        /// <summary>
+        /// Listará todas as classes, com as habilidades "linkadas"
+        /// </summary>
+        /// <returns>A lista com todas as classes com as habilidades</returns>
+        //List<Classe> ListarComHabilidades();
+
+        /// <summary>
+        /// Listará todas as classes, com os personagens onde estão inclusas
+        /// </summary>
+        /// <returns>A lista com todas as classes, com os personagens</returns>
+        List<Classe> ListarComPersonagem();
+    }
+}
---
+++ senai.hroads.webApi/senai.hroads.webApi/Interfaces/IHabilidadeRepository.cs
@@ -0,0 +1,58 @@
+﻿using senai.hroads.webApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.hroads.webApi.Interfaces
+{
+    /// <summary>
+    /// Interface responsável pela entidade "Habilidade"
+    /// </summary>
+    interface IHabilidadeRepository
+    {
+        /// <summary>
+        /// Lista todas as habilidades
+        /// </summary>
+        /// <returns>A lista com todas as habilidades</returns>
+        List<Habilidade> ListarTodos();
+
+        /// <summary>
+        /// Busca pelo ID uma habilidade em específica
+        /// </summary>
+        /// <param name="idHabilidade">ID da habilidade que será utilizada para a busca</param>
+        /// <returns>A habilidade buscada pela ID</returns>
+        Habilidade BuscarPorID(int idHabilidade);
+
+        /// <summary>
+        /// Registra uma nova habilidade
+        /// </summary>
+        /// <param name="novaHabilidade">Objeto utilizado para nomear a nova habilidade</param>
+        void Cadastrar(Habilidade novaHabilidade);
+
+        /// <summary>
+        /// Atualizará uma informação, sendo buscada pela URL
+        /// </summary>
+        /// <param name="idHabilidade">ID da habilidade que será utilizada para a identificação de qual ID que será atualizada</param>
+        /// <param name="habilidadeAtualizada">Objeto que estará no corpo da requisição que servirá para ser atualizado</param>
+        void AtualizarURL(int idHabilidade, Habilidade habilidadeAtualizada);
+
+        /// <summary>
+        /// Deletará uma habilidade
+        /// </summary>
+        /// <param name="idHabilidade">ID da habilidade que será utilizada para a remoção de uma informação</param>
+        void Deletar(int idHabilidade);
+
+        /// <summary>
+        /// Listará todas as habilidades, com tipos delas "linkadas"
+        /// </summary>
+        /// <returns>A lista com todas as habilidades com os tipos das habilidades</returns>
+        List<Habilidade> ListarComTipoHabilidades();
+
+        /// <summary>
+        /// Listará todas as habilidades, com as classes onde estão inclusas
+        /// </summary>
+        /// <returns>A lista com todas as habilidades, com as classes</returns>
+        List<Habilidade> ListarComClasseHabilidades();
+    }
+}
