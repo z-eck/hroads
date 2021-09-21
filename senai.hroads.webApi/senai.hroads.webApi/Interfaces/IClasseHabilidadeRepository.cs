b132e8cf28f171b7ab03e1201a79e3fa86300311
Zeck <61870100+z-eck@users.noreply.github.com>
2021-09-21 00:09:07 -0300

    Atualizado e Completinho
    Testado e funcional :D

---
+++ senai.hroads.webApi/senai.hroads.webApi/Interfaces/IClasseHabilidadeRepository.cs
@@ -0,0 +1,46 @@
+﻿using senai.hroads.webApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.hroads.webApi.Interfaces
+{
+    /// <summary>
+    /// Interface responsável pela entidade "ClasseHabilidade"
+    /// </summary>
+    interface IClasseHabilidadeRepository
+    {
+        /// <summary>
+        /// Lista todas as classes-habilidades
+        /// </summary>
+        /// <returns>A lista com todas as classes-habilidades</returns>
+        List<ClasseHabilidade> ListarTodos();
+
+        /// <summary>
+        /// Busca pelo ID uma classe-habilidade em específica
+        /// </summary>
+        /// <param name="idClasseHabilidade">ID da classe-habilidade que será utilizada para a busca</param>
+        /// <returns>A classe-habilidade buscada pela ID</returns>
+        ClasseHabilidade BuscarPorID(int idClasseHabilidade);
+
+        /// <summary>
+        /// Registra uma nova classe-habilidade
+        /// </summary>
+        /// <param name="novaClasseHabilidade">Objeto utilizado para nomear a nova classe-habilidade</param>
+        void Cadastrar(ClasseHabilidade novaClasseHabilidade);
+
+        /// <summary>
+        /// Atualizará uma informação, sendo buscada pela URL
+        /// </summary>
+        /// <param name="idClasseHabilidade">ID da classe-habilidade que será utilizada para a identificação de qual ID que será atualizada</param>
+        /// <param name="chAtualizada">Objeto que estará no corpo da requisição que servirá para ser atualizado</param>
+        void AtualizarURL(int idClasseHabilidade, ClasseHabilidade chAtualizada);
+
+        /// <summary>
+        /// Deletará uma classe-habilidade
+        /// </summary>
+        /// <param name="idClasseHabilidade">ID da classe-habilidade que será utilizada para a remoção de uma informação</param>
+        void Deletar(int idClasseHabilidade);
+    }
+}
---
+++ senai.hroads.webApi/senai.hroads.webApi/Interfaces/IClasseRepository.cs
@@ -0,0 +1,43 @@
+﻿using senai.hroads.webApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.hroads.webApi.Interfaces
+{
+    interface IClasseRepository
+    {
+        /// <summary>
+        /// Lista todas as classes
+        /// </summary>
+        /// <returns>A lista com todas as classes</returns>
+        List<Classe> ListarTodos();
+
+        /// <summary>
+        /// Busca pelo ID uma classe em específica
+        /// </summary>
+        /// <param name="idClasse">ID da classe que será utilizada para a busca</param>
+        /// <returns>A classe buscada pela ID</returns>
+        Classe BuscarPorID(int idClasse);
+
+        /// <summary>
+        /// Registra uma nova classe
+        /// </summary>
+        /// <param name="novaClasse">Objeto utilizado para nomear a nova classe</param>
+        void Cadastrar(Classe novaClasse);
+
+        /// <summary>
+        /// Atualizará uma informação, sendo buscada pela URL
+        /// </summary>
+        /// <param name="idClasse">ID da classe que será utilizada para a identificação de qual ID que será atualizada</param>
+        /// <param name="classeAtualizada">Objeto que estará no corpo da requisição que servirá para ser atualizado</param>
+        void AtualizarURL(int idClasse, Classe classeAtualizada);
+
+        /// <summary>
+        /// Deletará uma classe
+        /// </summary>
+        /// <param name="idClasse">ID da classe que será utilizada para a remoção de uma informação</param>
+        void Deletar(int idClasse);
+    }
+}
